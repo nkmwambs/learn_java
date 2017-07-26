334fb3279b9bbb7e8285b45f984e6234772df1c7
NKarisa <NKarisa@KEPIL136160121.CI.ORG>
2017-07-27 01:12:36 +0300

    Implementing ActionListener

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -9,16 +9,16 @@ package calculator;
  *
  * @author NKarisa
  */
-import java.awt.*;
+
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Calculator{
+public class Calculator implements ActionListener{
 
     /**
      * @param args the command line arguments
      */
-    JTextField num1,num2;
+    JTextField num1,num2,result;
     JButton plus,minus;
     JFrame frame;
     JPanel panel;
@@ -45,12 +45,45 @@ public class Calculator{
         
         num2 = new JTextField(20);
         num2.setBounds(10, 20, 50, 10);
-        panel.add(num2);   
+        panel.add(num2);
+        
+        result = new JTextField(20);
+        result.setBounds(10, 30, 50, 10);
+        panel.add(result);
         
         plus = new JButton("+");
-        plus.setBounds(10, 50, 60, 10);
+        plus.setBounds(10, 50, 60, 10); 
+        plus.addActionListener(this);
         panel.add(plus);
+        
+        minus = new JButton("-");
+        minus.setBounds(40, 50, 60, 10);
+        minus.addActionListener(this);
+        panel.add(minus);
+      
     }
+
+
+    public void actionPerformed(ActionEvent e){
+        int ans = 0;
+        
+        String s1 = num1.getText();
+        String s2 = num2.getText();
+        
+        int a = Integer.parseInt(s1);
+        int b = Integer.parseInt(s2);
+        
+        if(e.getSource() == plus){
+            ans = a+b;
+        }else if(e.getSource() == minus){
+            ans = a-b;
+        }
+        
+        String finalAnswer = String.valueOf(ans);
+        
+        result.setText(finalAnswer);
+    }   
+
     
     public static void main(String[] args) {
         // TODO code application logic here
