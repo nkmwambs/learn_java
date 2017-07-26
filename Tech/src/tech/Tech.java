1df4c5470ddf2db46d81cdf62f8fa9f4949f63aa
NKarisa <NKarisa@KEPIL136160121.CI.ORG>
2017-07-27 00:10:17 +0300

    Calculator

---
+++ Tech/src/tech/Tech.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tech;
+
+import myProduct.*;
+
+/**
+ *
+ * @author NKarisa
+ */
+//import myProduct.*;
+
+public class Tech {
+
+    public static void main(String[] args){
+        Product prod = new Product();
+        
+        prod.B();
+    }
+    
+}
